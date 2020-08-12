61276a8682d36b7a70b713a3751cfef2d9be197a
James Webb <james@ultra-horizon.com>
2020-08-12 11:34:43 +0100

    Introduce function to convert SQL row to dictionary

--- ldb/backend/BaseBackend.py
+++ ldb/backend/BaseBackend.py
@@ -1,19 +1,19 @@
-
 import logging as log
 
 import sqlalchemy
 
-from ..records import Device
 from ..records import Board
 from ..records import Core
-from ..records import Target
+from ..records import CorrolationTraces
+from ..records import Device
 from ..records import Experiment
+from ..records import ProgramBinary
+from ..records import StatisticTrace
 from ..records import TTraceSet
+from ..records import Target
 from ..records import TraceSetBlob
 from ..records import VariableValues
-from ..records import StatisticTrace
-from ..records import CorrolationTraces
-from ..records import ProgramBinary
+
 
 class BaseBackend(object):
     """
@@ -25,14 +25,21 @@ class BaseBackend(object):
         """
         Create a new connection to a database
         """
-        self._engine        = sqlalchemy.create_engine(path)
-        self._SessionMaker  = sqlalchemy.orm.sessionmaker(bind=self._engine)
-        self._session       = self._SessionMaker()
+        self._engine = sqlalchemy.create_engine(path)
+        self._SessionMaker = sqlalchemy.orm.sessionmaker(bind=self._engine)
+        self._session = self._SessionMaker()
 
-        self._autocommit    = [autocommit]
+        self._autocommit = [autocommit]
 
     def close(self):
         self._session.close()
+
+    @staticmethod
+    def row_to_dict(row):
+        d = {}
+        for column in row.__table__.columns:
+            d[column.name] = str(getattr(row, column.name))
+        return d
 
     @property
     def autocommit(self):
@@ -46,25 +53,22 @@ class BaseBackend(object):
     def autocommit(self, value):
         self._autocommit[0] = value
 
-
-    def pushAutoCommit(self,ac):
-        self._autocommit.insert(0,ac)
+    def pushAutoCommit(self, ac):
+        self._autocommit.insert(0, ac)
 
     def popAutoCommit(self):
-        if(len(self._autocommit) > 1):
+        if (len(self._autocommit) > 1):
             self._autocommit.pop(0)
         else:
             pass
-
 
     def _handleAutocommit(self):
         """
         Called at the end of each insert/remove/update function to
         handle automatic comitting of pending operations.
         """
-        if(self._autocommit):
+        if (self._autocommit):
             self.commit()
-
 
     def commit(self):
         """
@@ -72,14 +76,12 @@ class BaseBackend(object):
         """
         self._session.commit()
 
-
     def createNew(path):
         """
         Creates a new database, as per the records schema.
 
         Returns True if everything was successful, else False
         """
-
 
     def insertDevice(self, device):
         """
@@ -92,7 +94,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def insertBoard(self, board):
         """
         Insert a new board into the database, as described by the
@@ -104,8 +105,7 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
-    def insertCore(self, cpu ):
+    def insertCore(self, cpu):
         """
         Insert a new Core into the database, as described by the
         cpu parameter.
@@ -115,7 +115,6 @@ class BaseBackend(object):
         self._session.add(cpu)
         self._handleAutocommit()
         return None
-
 
     def insertTarget(self, target):
         """
@@ -128,7 +127,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def insertExperiment(self, experiment):
         """
         Insert a new experiment into the database, as described by the
@@ -139,7 +137,6 @@ class BaseBackend(object):
         self._session.add(experiment)
         self._handleAutocommit()
         return None
-
 
     def insertTTraceSet(self, ttraceset):
         """
@@ -152,7 +149,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def insertTraceSetBlob(self, tracesetblob):
         """
         Insert a new TraceSetBlob into the database, as described by the
@@ -164,7 +160,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def insertProgramBinary(self, binary):
         """
         Insert a new program binary into the database.
@@ -172,7 +167,6 @@ class BaseBackend(object):
         self._session.add(binary)
         self._handleAutocommit()
         return None
-
 
     def insertVariableValues(self, variableValues):
         """
@@ -185,7 +179,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def insertCorrolationTrace(self, corrolationTrace):
         """
         Insert a new CorrolationTraces object into the database, as described
@@ -197,7 +190,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-
     def getAllDevices(self):
         """
         Return an iterator which will iterate through all devices
@@ -205,14 +197,12 @@ class BaseBackend(object):
         """
         return self._session.query(Device).order_by(Device.id)
 
-
     def getAllBoards(self):
         """
-        Return an iterator which will iterate through all boards 
+        Return an iterator which will iterate through all boards
         in the database.
         """
         return self._session.query(Board).order_by(Board.id)
-
 
     def getAllCores(self):
         """
@@ -221,7 +211,6 @@ class BaseBackend(object):
         """
         return self._session.query(Core).order_by(Core.id)
 
-
     def getAllTargets(self):
         """
         Return an iterator which will iterate through all targets
@@ -229,14 +218,12 @@ class BaseBackend(object):
         """
         return self._session.query(Target).order_by(Target.id)
 
-
     def getAllStatisticTraces(self):
         """
-        Return an iterator which will iterate through all statistic traces 
+        Return an iterator which will iterate through all statistic traces
         in the database.
         """
         return self._session.query(StatisticTrace).order_by(StatisticTrace.id)
-
 
     def getAllExperiments(self):
         """
@@ -245,14 +232,12 @@ class BaseBackend(object):
         """
         return self._session.query(Experiment).order_by(Experiment.id)
 
-
     def getAllTTraceSets(self):
         """
         Return an iterator which will iterate through all TTraceSet objects
         in the database.
         """
         return self._session.query(TTraceSet).order_by(TTraceSet.id)
-
 
     def getAllTraceSetBlobs(self):
         """
@@ -261,7 +246,6 @@ class BaseBackend(object):
         """
         return self._session.query(TraceSetBlob).order_by(TraceSetBlob.id)
 
-
     def getAllVariableValues(self):
         """
         Return an iterator which will iterate through all VariableValues
@@ -269,14 +253,12 @@ class BaseBackend(object):
         """
         return self._session.query(VariableValues).order_by(VariableValues.id)
 
-
     def getAllCorrolationTraces(self):
         """
         Return an iterator which will iterate through all CorrolationTraces
         objects in the database.
         """
         return self._session.query(CorrolationTraces).order_by(CorrolationTraces.id)
-
 
     def getDeviceById(self, deviceId):
         """
@@ -287,7 +269,6 @@ class BaseBackend(object):
         """
         return self._session.query(Device).filter_by(id=deviceId).one_or_none()
 
-
     def getBoardById(self, boardId):
         """
         Return an instance of the Board class from the database with
@@ -296,7 +277,6 @@ class BaseBackend(object):
         :returns: None or Board
         """
         return self._session.query(Board).filter_by(id=boardId).one_or_none()
-
 
     def getCoreById(self, coreId):
         """
@@ -307,16 +287,14 @@ class BaseBackend(object):
         """
         return self._session.query(Core).filter_by(id=coreId).one_or_none()
 
-
     def getTargetById(self, targetId):
         """
         Return an instance of the Target class from the database with
         the supplied targetId.
 
-        :returns: None or Target 
+        :returns: None or Target
         """
         return self._session.query(Target).filter_by(id=targetId).one_or_none()
-
 
     def getDeviceByName(self, deviceName):
         """
@@ -327,7 +305,6 @@ class BaseBackend(object):
         """
         return self._session.query(Device).filter_by(name=deviceName).first()
 
-
     def getBoardByName(self, boardName):
         """
         Return an instance of the Board class from the database with
@@ -336,7 +313,6 @@ class BaseBackend(object):
         :returns: None or Board
         """
         return self._session.query(Board).filter_by(name=boardName).first()
-
 
     def getCoreByName(self, coreName):
         """
@@ -347,16 +323,14 @@ class BaseBackend(object):
         """
         return self._session.query(Core).filter_by(name=coreName).first()
 
-
     def getTargetByName(self, targetName):
         """
         Return an instance of the Target class from the database with
         the supplied targetName.
 
-        :returns: None or Target 
+        :returns: None or Target
         """
         return self._session.query(Target).filter_by(name=targetName).first()
-    
 
     def getExperimentById(self, experimentId):
         """
@@ -366,8 +340,7 @@ class BaseBackend(object):
         :returns: None or Experiment
         """
         return self._session.query(Experiment).filter_by(id=experimentId).one_or_none()
-    
-    
+
     def getStatisticTraceById(self, sid):
         """
         Return an instance of the StatisticTrace class from the database with
@@ -377,13 +350,11 @@ class BaseBackend(object):
         """
         return self._session.query(StatisticTrace).filter_by(id=sid).one_or_none()
 
-
     def getTargetsByDevice(self, deviceId):
         """
-        Return all of the targets where deviceid matches the supplied deviceId 
+        Return all of the targets where deviceid matches the supplied deviceId
         """
         return self._session.query(Target).filter_by(deviceid=deviceId).all()
-
 
     def getTargetsByCore(self, coreId):
         """
@@ -391,13 +362,11 @@ class BaseBackend(object):
         """
         return self._session.query(Target).filter_by(coreid=coreId).all()
 
-
     def getTargetsByBoard(self, boardId):
         """
         Return all of the targets where boardid matches the supplied boardId
         """
         return self._session.query(Target).filter_by(boardid=boardId).all()
-    
 
     def getExperimentsByTarget(self, targetId):
         """
@@ -405,10 +374,9 @@ class BaseBackend(object):
         corresponding to the supplied targetId
         """
         eids = self._session.query(TraceSetBlob.experimentId).filter_by(
-                targetId = targetId).distinct().all()
+            targetId=targetId).distinct().all()
         eids = [x[0] for x in eids]
         return self._session.query(Experiment).filter(Experiment.id.in_(eids))
-
 
     def getExperimentCountByTarget(self, targetId):
         """
@@ -416,20 +384,18 @@ class BaseBackend(object):
         corresponding to the supplied ID.
         """
         eids = self._session.query(TraceSetBlob.experimentId).filter_by(
-                targetId = targetId).distinct().all()
+            targetId=targetId).distinct().all()
         return len(eids)
-
 
     def getTargetsByExperiment(self, experimentId):
         """
         Return the set of targets for which there are results
-        corresponding to the supplied experimentId 
+        corresponding to the supplied experimentId
         """
         tids = self._session.query(TraceSetBlob.targetId).filter_by(
-                experimentId = experimentId).distinct().all()
+            experimentId=experimentId).distinct().all()
         tids = [x[0] for x in tids]
         return self._session.query(Target).filter(Target.id.in_(tids))
-
 
     def getExperimentByCatagoryAndName(self, catagory, name):
         """
@@ -439,10 +405,9 @@ class BaseBackend(object):
         :returns: None or Experiment
         """
         return self._session.query(Experiment).filter_by(
-            catagory = catagory,
-            name     = name
+            catagory=catagory,
+            name=name
         ).first()
-
 
     def getTTraceSetsById(self, ttraceSetId):
         """
@@ -455,17 +420,15 @@ class BaseBackend(object):
             id=ttraceSetId
         ).one_or_none()
 
-
     def getTTraceSetsByTargetAndExperiment(self, targetId, experimentId):
         """
         Return the set of TTraces matching the supplied target and experiment
         ids.
         """
         return self._session.query(TTraceSet).filter_by(
-            targetId     = targetId,
-            experimentId = experimentId
+            targetId=targetId,
+            experimentId=experimentId
         )
-
 
     def getCorrolationTraceByTargetAndExperiment(self, targetId, experimentId):
         """
@@ -473,10 +436,9 @@ class BaseBackend(object):
         experiment ids.
         """
         return self._session.query(CorrolationTraces).filter_by(
-            targetId     = targetId,
-            experimentId = experimentId
+            targetId=targetId,
+            experimentId=experimentId
         )
-
 
     def getTraceSetBlobById(self, traceSetBlobId):
         """
@@ -486,18 +448,16 @@ class BaseBackend(object):
             id=traceSetBlobId
         ).one_or_none()
 
-
     def getTraceSetBlobByTargetAndExperiment(self, tgt_id, exp_id):
         """
         Return all of the trace set blobs associated with the
         supplied eperiment and target IDs
         """
-        return self._session.query(TraceSetBlob).filter_by (
-            targetId    =tgt_id,
+        return self._session.query(TraceSetBlob).filter_by(
+            targetId=tgt_id,
             experimentId=exp_id
         )
-    
-    
+
     def getCorrolationTraceById(self, corrolationTraceId):
         """
         Return the CorrolationTraces object with the supplied ID
@@ -506,10 +466,9 @@ class BaseBackend(object):
             id=corrolationTraceId
         ).one_or_none()
 
-
     def getCorrolationTraceByAll(self,
-        targetId, experimentId, inputTraceSetIds, inputVarIds, corrType
-        ):
+                                 targetId, experimentId, inputTraceSetIds, inputVarIds, corrType
+                                 ):
         """
         Tries to find a corrolation trace corresponding to all of the
         supplied inputs. Returns it if found, otherwise None
@@ -517,9 +476,9 @@ class BaseBackend(object):
 
         q = self._session.query(CorrolationTraces).filter(
             sqlalchemy.and_(
-                CorrolationTraces.targetId      == targetId,
-                CorrolationTraces.experimentId  == experimentId,
-                CorrolationTraces.corrType      == corrType
+                CorrolationTraces.targetId == targetId,
+                CorrolationTraces.experimentId == experimentId,
+                CorrolationTraces.corrType == corrType
             )
         )
 
@@ -527,19 +486,18 @@ class BaseBackend(object):
 
             inSetIds = set([b.id for b in candidate.inputTraceSets])
 
-            if(inSetIds == inputTraceSetIds):
-                
+            if (inSetIds == inputTraceSetIds):
+
                 inVarIds = set([v.id for v in candidate.inputVariables])
 
-                if(inVarIds == inputVarIds):
+                if (inVarIds == inputVarIds):
                     return candidate
 
         return None
 
-
     def getProgramBinaryById(self, pid):
         pbin = self._session.query(ProgramBinary).filter_by(
-            id =pid
+            id=pid
         )
         return pbin.first()
 
@@ -548,14 +506,13 @@ class BaseBackend(object):
         Return all program binaries corresponding to the supplied
         target and experiment ID.
         """
-        pbin = self._session.query(ProgramBinary).filter (
+        pbin = self._session.query(ProgramBinary).filter(
             sqlalchemy.and_(
-                ProgramBinary.targetId      == tgtId,
-                ProgramBinary.experimentId  == expId
+                ProgramBinary.targetId == tgtId,
+                ProgramBinary.experimentId == expId
             )
         )
         return pbin.first()
-        
 
     def removeProgramBinary(self, pbin):
         """
@@ -563,7 +520,6 @@ class BaseBackend(object):
         """
         self._session.delete(pbin)
         self._handleAutocommit()
-
 
     def removeTTraceSet(self, ttraceSetId):
         """
@@ -576,7 +532,6 @@ class BaseBackend(object):
         self._session.delete(ttraceSet)
 
         self._handleAutocommit()
-
 
     def removeTraceSetBlob(self, traceSetBlobId):
         """
@@ -591,8 +546,7 @@ class BaseBackend(object):
         self._session.delete(traceSetBlob)
 
         self._handleAutocommit()
-    
-    
+
     def removeCorrolationTrace(self, corrolationTraceId):
         """
         Remove the corrolationTrace with the supplied id
@@ -607,18 +561,16 @@ class BaseBackend(object):
 
         self._handleAutocommit()
 
-
     def removeDevice(self, deviceId):
         """
         Remove the device from the database with the supplied Id,
         along with all experimental data from the database which
         points to that device.
         """
-        assert(False)
+        assert (False)
 
         self._handleAutocommit()
         return None
-
 
     def removeBoard(self, boardId):
         """
@@ -626,11 +578,10 @@ class BaseBackend(object):
         along with all experimental data from the database which
         points to that board.
         """
-        assert(False)
+        assert (False)
 
         self._handleAutocommit()
         return None
-
 
     def removeCore(self, coreId):
         """
@@ -638,11 +589,10 @@ class BaseBackend(object):
         along with all experimental data from the database which
         points to that Core.
         """
-        assert(False)
+        assert (False)
 
         self._handleAutocommit()
         return None
-
 
     def removeTarget(self, targetId):
         """
@@ -653,12 +603,12 @@ class BaseBackend(object):
         tgt = self.getTargetById(targetId)
 
         # Remove ttests
-        ttests = self._session.query(TTraceSet).filter_by(targetId = tgt.id)
+        ttests = self._session.query(TTraceSet).filter_by(targetId=tgt.id)
         for t in ttests:
             self._session.delete(t)
 
         # Remove trace blobs
-        blobs = self._session.query(TraceSetBlob).filter_by(targetId = tgt.id)
+        blobs = self._session.query(TraceSetBlob).filter_by(targetId=tgt.id)
         for b in blobs:
             self._session.delete(b)
 
@@ -668,7 +618,6 @@ class BaseBackend(object):
         self._handleAutocommit()
         return None
 
-    
     def removeExperiment(self, experimentId):
         """
         Remove the experiment from the database with the supplied Id,
@@ -677,7 +626,7 @@ class BaseBackend(object):
         """
 
         tracesets = self._session.query(TTraceSet).filter_by(
-            experimentId = experimentId
+            experimentId=experimentId
         ).all()
 
         for t in tracesets:
@@ -685,19 +634,18 @@ class BaseBackend(object):
             self._session.delete(t)
 
         corrtraces = self._session.query(CorrolationTraces).filter_by(
-            experimentId = experimentId
+            experimentId=experimentId
         ).all()
-        
+
         for t in corrtraces:
             log.info("Remove corrolation trace id=%d" % t.id)
             self._session.delete(t)
 
         experiment = self.getExperimentById(experimentId)
-        
+
         log.info("Remove experiment id=%d" % experiment.id)
 
         self._session.delete(experiment)
-        
+
         self._handleAutocommit()
         return None
-
